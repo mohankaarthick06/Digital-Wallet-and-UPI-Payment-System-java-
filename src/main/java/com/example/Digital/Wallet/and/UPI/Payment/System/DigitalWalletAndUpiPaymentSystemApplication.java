f98294c93f703a47bde781ca85733f33770a0235
Mohan <mohankaarthick06@gmail.com>
2025-10-29 11:17:53 +0530

    Initial Commit

---
+++ src/main/java/com/example/Digital/Wallet/and/UPI/Payment/System/DigitalWalletAndUpiPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Digital.Wallet.and.UPI.Payment.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalWalletAndUpiPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalWalletAndUpiPaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Digital/Wallet/and/UPI/Payment/System/DigitalWalletAndUpiPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Digital.Wallet.and.UPI.Payment.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWalletAndUpiPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
