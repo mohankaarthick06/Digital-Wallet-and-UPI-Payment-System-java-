62efa6c12c696a507b1e8488f2f8f6faa03c8f83
Mohan <mohankaarthick06@gmail.com>
2025-10-29 11:56:21 +0530

    bill details

--- src/main/java/com/example/Digital/Wallet/and/UPI/Payment/System/model/Bill.java
+++ src/main/java/com/example/Digital/Wallet/and/UPI/Payment/System/model/Bill.java
@@ -1,5 +1,27 @@
 package com.example.Digital.Wallet.and.UPI.Payment.System.model;
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "bills")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Bill {
-    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Biller biller;
+
+    @ManyToOne
+    private Wallet wallet;
+
+    private BigDecimal amount;
+    private boolean paid = false;
+    private LocalDateTime dueDate;
+    private LocalDateTime paidAt;
 }
+
