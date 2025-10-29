78629e94a23171c247e93da17702981e8437d37c
Mohan <mohankaarthick06@gmail.com>
2025-10-29 11:59:56 +0530

    Adding Biller entity with fields

--- src/main/java/com/example/Digital/Wallet/and/UPI/Payment/System/model/Biller.java
+++ src/main/java/com/example/Digital/Wallet/and/UPI/Payment/System/model/Biller.java
@@ -1,5 +1,13 @@
 package com.example.Digital.Wallet.and.UPI.Payment.System.model;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "billers")
 public class Biller {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String code; 
+    public Biller() {}
     
 }
